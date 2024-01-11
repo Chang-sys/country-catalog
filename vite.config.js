76ae982943818ef3e0ab5347a8186cad51ab68fa
Chang-sys <chhengchang90@gmail.com>
2024-01-11 17:06:33 +0700

    fix data on idd, native name, alt Country name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/country-catalog/',
-  // publicDir: 'country_catelog',
   plugins: [ vue() ],
   resolve: {
     alias: {
